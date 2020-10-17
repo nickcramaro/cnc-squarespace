0d0e9f9d663fd84b1bb23ab19f06604521074093
Nick Cramaro <nickcramaro@gmail.com>
2020-10-17 17:44:36 -0400

    Modal fix

--- scripts/site.js
+++ scripts/site.js
@@ -28,11 +28,12 @@
 
   // Get the button that opens the modal
   var allButtons = document.querySelectorAll('button[class=cta]');
+  var body = document.querySelector('body');
   
   for (var i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener('click', function() {
-      console.log('clicked');
       modal.style.display = "block";
+      body.style.overflow = "hidden";
     });
   }
 
@@ -42,12 +43,14 @@
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
+    body.style.overflow = "unset";
   }
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      body.style.overflow = "unset";
     }
   }
 
