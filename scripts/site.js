acbc38b775151dcae014ebcbece4fd8c567346a2
Nick Cramaro <nickcramaro@gmail.com>
2020-10-17 15:20:06 -0400

    Added modal

--- scripts/site.js
+++ scripts/site.js
@@ -23,4 +23,32 @@
   // The event subscription that reloads images on resize
   window.addEventListener('resize', loadAllImages);
 
+  // Get the modal
+  var modal = document.getElementById("contact-modal");
+
+  // Get the button that opens the modal
+  var allButtons = document.querySelectorAll('button[class=cta]');
+  
+  for (var i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener('click', function() {
+      console.log('clicked');
+      modal.style.display = "block";
+    });
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 }());
